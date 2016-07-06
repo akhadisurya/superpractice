dad89f309af4c87ebe52077f33ed7c179dbd3207
Arvin <akhadisurya@gmail.com>
2016-07-06 14:06:59 -0700

    Yes

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -5,7 +5,7 @@ class OrganizationsController < ApplicationController
   # GET /organizations.json
 
   def organization_params
-      params.require(:organization).permit(:name, [etc...], :logo)
+      params.require(:organization).permit(:name,:logo)
   end
 
   def index
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
   Rails.application.routes.default_url_options[:host] = 'superpractice.herokuapp.com'
   config.action_mailer.default_url_options = { :host => 'superpractice.herokuapp.com' }
 
@@ -93,6 +94,7 @@ Rails.application.configure do
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
   }
+
   config.paperclip_defaults = {
   storage: :s3,
   s3_host_name: 's3-us-west-1.amazonaws.com',
